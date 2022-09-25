d545f4ef539d39ca10a5945f0d93186b1ed188c8
Jeverson Mozer <jeversonsilveira@outlook.com>
2022-09-24 21:40:18 -0300

    GetApiCep

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -7,7 +7,13 @@
     <label for="senha">Senha</label>
     <input type="password" name="senha" id="senha" v-model="senha" />
     <label for="cep">Cep</label>
-    <input type="text" name="cep" id="cep" v-model="cep" />
+    <input
+      type="text"
+      name="cep"
+      id="cep"
+      v-model="cep"
+      @keyup="preencherCep"
+    />
     <label for="rua">Rua</label>
     <input type="text" name="rua" id="rua" v-model="rua" />
     <label for="numero">Numero</label>
@@ -26,6 +32,7 @@
 
 <script>
 import { mapFields } from "@/helpers.js";
+import { getCep } from "@/services.js";
 
 export default {
   computed: {
@@ -44,6 +51,19 @@ export default {
       base: "usuario",
       mutation: "UPDATE_USUARIO",
     }),
+  },
+  methods: {
+    preencherCep() {
+      const cep = this.cep.replace(/\D/g, "");
+      if (cep.length === 8) {
+        getCep(cep).then((response) => {
+          this.rua = response.data.logradouro;
+          this.bairro = response.data.bairro;
+          this.estado = response.data.uf;
+          this.cidade = response.data.localidade;
+        });
+      }
+    },
   },
 };
 </script>
