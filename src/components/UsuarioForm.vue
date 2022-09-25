87df4a8d5bebf9707db47de2405d5cdc7d75eb96
Jeverson Mozer <jeversonsilveira@outlook.com>
2022-09-25 11:22:27 -0300

    FinalizarCompra

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -1,29 +1,25 @@
 <template>
   <form>
-    <label for="nome">Nome</label>
-    <input type="text" name="nome" id="nome" v-model="nome" />
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" v-model="email" />
-    <label for="senha">Senha</label>
-    <input type="password" name="senha" id="senha" v-model="senha" />
+    <div class="usuario" v-if="mostrarDadosLogin">
+      <label for="nome">Nome</label>
+      <input id="nome" name="nome" type="text" v-model="nome">
+      <label for="email">Email</label>
+      <input id="email" name="email" type="email" v-model="email">
+      <label for="senha">Senha</label>
+      <input id="senha" name="senha" type="password" v-model="senha">
+    </div>
     <label for="cep">Cep</label>
-    <input
-      type="text"
-      name="cep"
-      id="cep"
-      v-model="cep"
-      @keyup="preencherCep"
-    />
+    <input id="cep" name="cep" type="text" v-model="cep" @keyup="preencherCep">
     <label for="rua">Rua</label>
-    <input type="text" name="rua" id="rua" v-model="rua" />
+    <input id="rua" name="rua" type="text" v-model="rua">
     <label for="numero">Numero</label>
-    <input type="text" name="numero" id="numero" v-model="numero" />
+    <input id="numero" name="numero" type="text" v-model="numero">
     <label for="bairro">Bairro</label>
-    <input type="text" name="bairro" id="bairro" v-model="bairro" />
+    <input id="bairro" name="bairro" type="text" v-model="bairro">
     <label for="cidade">Cidade</label>
-    <input type="text" name="cidade" id="cidade" v-model="cidade" />
+    <input id="cidade" name="cidade" type="text" v-model="cidade">
     <label for="estado">Estado</label>
-    <input type="text" name="estado" id="estado" v-model="estado" />
+    <input id="estado" name="estado" type="text" v-model="estado">
     <div class="button">
       <slot></slot>
     </div>
@@ -46,34 +42,43 @@ export default {
         "numero",
         "bairro",
         "cidade",
-        "estado",
+        "estado"
       ],
       base: "usuario",
-      mutation: "UPDATE_USUARIO",
+      mutation: "UPDATE_USUARIO"
     }),
+    mostrarDadosLogin() {
+      return !this.$store.state.login || this.$route.name === "usuario-editar";
+    }
   },
   methods: {
     preencherCep() {
       const cep = this.cep.replace(/\D/g, "");
       if (cep.length === 8) {
-        getCep(cep).then((response) => {
+        getCep(cep).then(response => {
           this.rua = response.data.logradouro;
           this.bairro = response.data.bairro;
           this.estado = response.data.uf;
           this.cidade = response.data.localidade;
         });
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
-form {
+form,
+.usuario {
   display: grid;
   grid-template-columns: 80px 1fr;
   align-items: center;
 }
+
+.usuario {
+  grid-column: 1 / 3;
+}
+
 .button {
   grid-column: 2;
   margin-top: 10px;
